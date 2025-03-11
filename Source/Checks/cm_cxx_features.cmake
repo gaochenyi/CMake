c338f412efb0f8f4ee303bd3933d92174838924c
Chen-Yi Gao <32761974+gaochenyi@users.noreply.github.com>
2025-03-10 22:53:57 -0700

    MOD for clock skew

--- Source/Checks/cm_cxx_features.cmake
+++ Source/Checks/cm_cxx_features.cmake
@@ -63,6 +63,9 @@ function(cm_check_cxx_feature name)
     string(REGEX REPLACE "[^\n]*ld: warning: [^\n]*" "" check_output "${check_output}")
     # Filter out distcc.
     string(REGEX REPLACE "[^\n]*distcc\\[[0-9]+\\][^\n]*[Ww]arning:[^\n]*" "" check_output "${check_output}")
+    # Filter warnings about clock skew
+    string(REGEX REPLACE "[^\n]*[Ww]arning[^\n]*Clock skew[^\n]*" "" check_output "${check_output}")
+    string(REGEX REPLACE "[^\n]*[Ww]arning[^\n]*modification time[^\n]*in the future[^\n]*" "" check_output "${check_output}")
     # If using the feature causes warnings, treat it as broken/unavailable.
     if(check_output MATCHES "(^|[ :])[Ww][Aa][Rr][Nn][Ii][Nn][Gg]")
       set(CMake_HAVE_CXX_${FEATURE} OFF CACHE INTERNAL "TRY_COMPILE" FORCE)
